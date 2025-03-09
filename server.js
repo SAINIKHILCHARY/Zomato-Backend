97031d736171e7935fe662b9e256ebbd3e988a80
sainikhil <madupusainikhil@gmail.com>
2025-03-09 11:10:42 +0530

    Initial commit

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  "https://zomato-frontend-lake.vercel.app/"
 ];
 
 app.use(cors({
